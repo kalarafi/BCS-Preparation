6a85614651c83e150440a592510b92a878d50d5c
Md. Rafi <65470604+kalarafi@users.noreply.github.com>
2025-10-01 03:11:04 +0600

    initial commit

---
+++ java/com/isl/bcspreparation/BottomSheetFragment.java
@@ -0,0 +1,30 @@
+package com.isl.bcspreparation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    public BottomSheetFragment () {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.termandcond,container,false);
+    }
+}
